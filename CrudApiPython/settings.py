6ece25091d6fd4f639fe2c8e557efb0fa2dac7dc
Roshan Yadav <81505163+Roshan-py@users.noreply.github.com>
2021-06-28 21:13:22 +0530

    Update settings.py

--- CrudApiPython/settings.py
+++ CrudApiPython/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+REST_FRAMEWORK={
+    'DEFAULT_RENDERER_CLASSES':('rest_framework.renderers.JSONRenderer',)
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
